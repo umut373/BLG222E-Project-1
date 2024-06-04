13407c09e64cacd527e45d0f4fce02728a6bdae8
umut373 <umut373.uz@gmail.com>
2024-06-05 01:34:03 +0300

    BLG222E project 1

---
+++ src/InstructionRegister.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.03.2024 16:11:36
+// Design Name: 
+// Module Name: InstructionRegister
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module InstructionRegister(I, Write, LH, Clock, IROut);
+    input wire [7:0] I;
+    input wire Write;
+    input wire LH;
+    input wire Clock;
+    
+    output reg [15:0] IROut;
+    
+    initial begin
+        IROut = 16'b0;
+    end
+    
+    always @(posedge Clock) begin
+        if(Write) begin
+            case(LH)
+                1'b0: IROut[7:0] = I;
+                1'b1: IROut[15:8] = I;
+            endcase
+        end
+    end
+    
+endmodule
